b1402a9226fa450d1129c931701a2942f9f256ce
kimseongho0519 <rlatjdgh1283@naver.com>
2022-03-23 01:21:58 +0900

    대문페이지와 자기소개 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
-# Create your views here.
+# 가장 최근에 적은 포스트부터 나열되게함.
+def index(request):
+    posts = Post.objects.all().order_by('-pk')
+
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts' : posts,
+        }
+    )
+
+def single_post_page(request, pk):
+    posts = Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        'blog/single_post_page.html',
+        {
+            'posts' : posts,
+        }
+    )
