c24345e962b5ce3daa81c4af9e67076b793e3660
kimseongho0519 <rlatjdgh1283@naver.com>
2022-03-25 17:01:12 +0900

    댓글 폼으로 포스트 상세페이지에서 댓글 작성

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,9 @@ from urllib import response
 from django.shortcuts import render ,redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import get_object_or_404
 from .models import Post, Category, Tag
+from .forms import CommentForm
 from django.core.exceptions import PermissionDenied
 from django.utils.text import slugify
 class PostList(ListView):
@@ -24,7 +26,8 @@ class PostDetail(DetailView):
         context = super(PostDetail, self).get_context_data()
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
-        
+        context['comment_form'] = CommentForm
+
         return context
 
 class PostCreate(LoginRequiredMixin,CreateView):
@@ -134,7 +137,22 @@ def tag_page(request, slug):
         }
     )
 
+def new_comment(request, pk):
+    if request.user.is_authenticated:
+        post = get_object_or_404(Post, pk=pk)
 
+        if request.method == 'POST':
+            comment_form = CommentForm(request.POST)
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.post = post
+                comment.author = request.user
+                comment.save()
+                return redirect(comment.get_absolute_url())
+        else:
+            return redirect(post.get_absolute_url())
+    else:
+        raise PermissionDenied
 
     
     # 가장 최근에 적은 포스트부터 나열되게함.
