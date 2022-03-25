d6eb7d8fc486c415928bd44c4b84235b7be73238
kimseongho0519 <rlatjdgh1283@naver.com>
2022-03-25 10:03:36 +0900

    django-crispy-forms 적용하기

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'crispy_forms',
     'blog',
     'single_pages',
 ]
@@ -133,3 +134,4 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
