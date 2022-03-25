34bf8397fa25d3bcf3a13fa1dd3fdf8e9e2c5b0b
kimseongho0519 <rlatjdgh1283@naver.com>
2022-03-25 10:32:23 +0900

    Content 입력란에 마크다운 적용하기

--- do_it_django_prj/urls.py
+++ do_it_django_prj/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [  
+
+    path('markdownx/',include('markdownx.urls')),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('', include('single_pages.urls')),
